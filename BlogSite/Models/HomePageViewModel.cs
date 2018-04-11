a00150e728b7758f1406ac76fcf02cd57f5c0e9b
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-04-10 21:56:50 -0400

    Initial commit

---
+++ BlogSite/Models/HomePageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BlogSite.Data;
+
+namespace BlogSite.Models
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<BlogPost> Posts { get; set; }
+        public int? NewerPage { get; set; }
+        public int? OlderPage { get; set; }
+    }
+}
